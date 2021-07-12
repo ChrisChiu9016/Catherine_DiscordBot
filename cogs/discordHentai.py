38a9d0ac2b050312817b7d593032869fc0926d29
ChrisChiu9016 <chris65126@gmail.com>
2021-07-12 12:56:37 +0800

    Update discordHentai.py

--- cogs/discordHentai.py
+++ cogs/discordHentai.py
@@ -6,12 +6,12 @@ from core.classes import Cog_Extension
 import hentai
 
 class discordHentai(Cog_Extension):
-  @commands.command(aliases=['隨機本本','rH'])
+  @commands.command(aliases=['隨機本本','rh'])
   async def randomHentai(self, ctx):
     r_id = hentai.Utils.get_random_id()
     doujin = hentai.Hentai(r_id)
     
-    embed=discord.Embed(title=doujin.title, description=r_id, url=doujin.url)
+    embed=discord.Embed(title=str(doujin.title), description=r_id, url=doujin.url)
     embed.set_thumbnail(url=doujin.thumbnail)
     embed.add_field(name="Tags", value=str([tag.name for tag in doujin.tag]).replace('\'','').replace('[','').replace(']',''), inline=False)
     embed.add_field(name="Pages", value=doujin.num_pages, inline=False)
