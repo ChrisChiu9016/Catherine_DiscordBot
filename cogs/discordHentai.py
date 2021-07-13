b0f84eb2bdc0e3fae2b21da910efa3380cefa223
ChrisChiu9016 <chris65126@gmail.com>
2021-07-13 23:45:44 +0800

    指定本本函式更新

--- cogs/discordHentai.py
+++ cogs/discordHentai.py
@@ -35,6 +35,9 @@ class discordHentai(Cog_Extension):
 	#指定本子
 	@commands.group(aliases=['hid'])
 	async def getHentai(self, ctx, id:int):
+		await ctx.send('請指定參數:simple/show')
+	@getHentai.command(aliases=['s'])
+	async def simple(self, ctx):
 		if ctx.invoked_subcommand is None:
 			if(hentai.Hentai.exists(id)):
 				doujin = hentai.Hentai(id)
@@ -44,6 +47,23 @@ class discordHentai(Cog_Extension):
 				embed.add_field(name="Tags", value=str([tag.name for tag in doujin.tag]).replace('\'','').replace('[','').replace(']',''), inline=False)
 				embed.add_field(name="Pages", value=doujin.num_pages, inline=False)
 				await ctx.send(embed=embed)
+				for url in doujin.image_urls:
+					await ctx.send(url)
+			else:
+				ctx.send(f'此ID不存在，請重試。')
+	@getHentai.command()
+	async def show(self, ctx):
+		if ctx.invoked_subcommand is None:
+			if(hentai.Hentai.exists(id)):
+				doujin = hentai.Hentai(id)
+				
+				embed=discord.Embed(title=str(doujin.title()), description=id, url=doujin.url)
+				embed.set_thumbnail(url=doujin.thumbnail)
+				embed.add_field(name="Tags", value=str([tag.name for tag in doujin.tag]).replace('\'','').replace('[','').replace(']',''), inline=False)
+				embed.add_field(name="Pages", value=doujin.num_pages, inline=False)
+				await ctx.send(embed=embed)
+				for url in doujin.image_urls:
+					await ctx.send(url)
 			else:
 				ctx.send(f'此ID不存在，請重試。')
 
